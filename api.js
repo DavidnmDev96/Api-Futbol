51ef6d5e56cc64ba1dbaacb7e22747edeb611b6a
Davidnaranjodaw <85646820+davidnaranjodaw@users.noreply.github.com>
2024-05-02 21:03:01 +0000

    eqeqe

--- api.js
+++ api.js
@@ -1,74 +1,83 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const app = express();
+const bodyParser = require('body-parser');
  
 // Configura aquí tus detalles de conexión a la base de datos SQLite
 const db = new sqlite3.Database("./db/database.sqlite");
- 
+app.use(bodyParser.json());
 // Endpoint para obtener los jugadores de un equipo específico por el nombre del equipo
-app.post("/jugadores/:equipo", (req, res) => {
+app.post("/jugadores", async (req, res) => {
   try {
-    const { equipo } = req.params;
-    console.log(equipo); // Obtén el nombre del equipo de los parámetros de la URL;  // Obtén el nombre del equipo de los parámetros de la URL
+    const equipo = req.body.equipo; // Obtén el nombre del equipo de los parámetros de la URL
+    console.log(req.body); // Obtén el nombre del equipo de los parámetros de la URL;  // Obtén el nombre del equipo de los parámetros de la URL
     const query = `SELECT DISTINCT p.player_name
-    FROM Player p
-    JOIN Match m ON (
-        p.player_api_id = m.home_player_1 OR
-        p.player_api_id = m.home_player_2 OR
-        p.player_api_id = m.home_player_3 OR
-        p.player_api_id = m.home_player_4 OR
-        p.player_api_id = m.home_player_5 OR
-        p.player_api_id = m.home_player_6 OR
-        p.player_api_id = m.home_player_7 OR
-        p.player_api_id = m.home_player_8 OR
-        p.player_api_id = m.home_player_9 OR
-        p.player_api_id = m.home_player_10 OR
-        p.player_api_id = m.home_player_11
-    )
-    JOIN Team t ON t.team_api_id = m.home_team_api_id
-    WHERE t.team_long_name = ?
-    AND strftime('%Y', m.date) = '2016' -- Filtrar por el año 2016
-    AND strftime('%Y', m.date) = (SELECT strftime('%Y', MAX(date)) FROM Match)
-    UNION
-    SELECT DISTINCT p.player_name
-    FROM Player p
-    JOIN Match m ON (
-        p.player_api_id = m.away_player_1 OR
-        p.player_api_id = m.away_player_2 OR
-        p.player_api_id = m.away_player_3 OR
-        p.player_api_id = m.away_player_4 OR
-        p.player_api_id = m.away_player_5 OR
-        p.player_api_id = m.away_player_6 OR
-        p.player_api_id = m.away_player_7 OR
-        p.player_api_id = m.away_player_8 OR
-        p.player_api_id = m.away_player_9 OR
-        p.player_api_id = m.away_player_10 OR
-        p.player_api_id = m.away_player_11
-    )
-    JOIN Team t ON t.team_api_id = m.away_team_api_id
-    WHERE t.team_long_name = ?
-    AND strftime('%Y', m.date) = '2016'
-    AND strftime('%Y', m.date) = (SELECT strftime('%Y', MAX(date)) FROM Match);`;
-    // Consulta SQL para seleccionar jugadores por equipo
-    db.all(query, [equipo], (err, rows) => {
+       FROM Player p
+      JOIN Match m ON (
+           p.player_api_id = m.home_player_1 OR
+          p.player_api_id = m.home_player_2 OR
+           p.player_api_id = m.home_player_3 OR
+           p.player_api_id = m.home_player_4 OR
+          p.player_api_id = m.home_player_5 OR
+            p.player_api_id = m.home_player_6 OR
+           p.player_api_id = m.home_player_7 OR
+            p.player_api_id = m.home_player_8 OR
+           p.player_api_id = m.home_player_9 OR
+           p.player_api_id = m.home_player_10 OR
+           p.player_api_id = m.home_player_11
+       )
+        JOIN Team t ON t.team_api_id = m.home_team_api_id
+       WHERE t.team_long_name = ?
+       AND strftime('%Y', m.date) = (SELECT strftime('%Y', MAX(date)) FROM Match)
+        UNION
+        SELECT DISTINCT p.player_name
+       FROM Player p
+       JOIN Match m ON (
+          p.player_api_id = m.away_player_1 OR
+           p.player_api_id = m.away_player_2 OR
+           p.player_api_id = m.away_player_3 OR
+         p.player_api_id = m.away_player_4 OR
+          p.player_api_id = m.away_player_5 OR
+           p.player_api_id = m.away_player_6 OR
+          p.player_api_id = m.away_player_7 OR
+           p.player_api_id = m.away_player_8 OR
+            p.player_api_id = m.away_player_9 OR
+           p.player_api_id = m.away_player_10 OR
+          p.player_api_id = m.away_player_11
+      )
+        JOIN Team t ON t.team_api_id = m.away_team_api_id
+       WHERE t.team_long_name = ?
+         AND strftime('%Y', m.date) = (SELECT strftime('%Y', MAX(date)) FROM Match);`;
+
+    db.all(query, [equipo], async (err, rows) => {
       if (err) {
         console.error(err);
-        res.status(500).send("Error en el servidor"); // Maneja errores potenciales
+        res.status(500).send("Error en el servidor"); 
         return;
       }
-      res.json(rows); // Envía los jugadores como respuesta en formato JSON
+      const playersObject = {};
+      rows.forEach((row, index) => {
+        // Obtener el nombre del jugador de la fila actual
+        const playerName = row.player_name;
+
+        // Crear una propiedad para cada jugador en el objeto de jugadores
+        playersObject[`player_${index + 1}`] = playerName;
+    });
+ 
+        res.json(playersObject); 
+       
+    
     });
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error en el servidor"); // Maneja errores potenciales
+    res.status(500).send("Error en el servidor"); 
   }
 });
  
+ 
+ 
 
-
-// El servidor escucha en el puerto 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
- 
